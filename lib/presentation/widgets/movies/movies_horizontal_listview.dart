b9977937f851cb09702efe89154cc783002ed98f
Adrián Primo <adrianprimob@gmail.com>
2024-05-31 00:43:21 +0200

    HumanFormat

--- lib/presentation/widgets/movies/movies_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movies_horizontal_listview.dart
@@ -1,3 +1,4 @@
+import 'package:cinemapedia/config/helpers/human_format.dart';
 import 'package:flutter/material.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:animate_do/animate_do.dart';
@@ -126,24 +127,25 @@ class _Slide extends StatelessWidget {
               style: Theme.of(context).textTheme.titleSmall,
             ),
           ),
-          Row(
-            children: [
-              Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
-              const SizedBox(
-                width: 5,
-              ),
-              Text(
-                movie.voteAverage.toString(),
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyMedium
-                    ?.copyWith(color: Colors.yellow.shade800),
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Text(movie.popularity.toString(), style: Theme.of(context).textTheme.bodySmall),
-            ],
+          SizedBox(
+            width: 150,
+            child: Row(
+              children: [
+                Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
+                const SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  movie.voteAverage.toString(),
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyMedium
+                      ?.copyWith(color: Colors.yellow.shade800),
+                ),
+                const Spacer(),
+                Text(HumanFormats.number(movie.popularity), style: Theme.of(context).textTheme.bodyMedium),
+              ],
+            ),
           )
         ],
       ),
